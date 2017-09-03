e85ae5a11ffd2b8a6caa72c1c03cd4c64c33544e
Liu Cong <liucong@pingcap.com>
2017-09-03 14:56:44 +0800

    Added align test

---
+++ src/github.com/pingcap/analysys/calculate_test.go
@@ -0,0 +1,14 @@
+package analysys
+
+import (
+	"testing"
+	"unsafe"
+)
+
+func TestAlign(t *testing.T) {
+	var row Row
+	var info RowInfo
+	if int(unsafe.Sizeof(info)) != int(row.PersistSize()) {
+		t.Fatal("row mem not aligned: %d != %d", int(unsafe.Sizeof(info)), int(row.PersistSize()))
+	}
+}
