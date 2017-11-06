af3b1df4333a671e201ddef5eb531f377010b333
Liu Cong <liucong@pingcap.com>
2017-11-07 01:17:49 +0800

    Add Magic-Patch cmake scripts in CH.

--- ch-connector/delta/dbms/src/Magic/JNI/Version.cpp
+++ ch-connector/delta/dbms/src/Magic/JNI/Version.cpp
@@ -2,5 +2,5 @@
 
 JNIEXPORT jstring JNICALL Java_pingcap_com_MagicProto_version(JNIEnv *env, jobject obj) {
     static const char *version = "v0.1";
-    return (*env)->NewStringUTF(env, version);
+    return env->NewStringUTF(version);
 }
