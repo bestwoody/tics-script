0ecf7149ae4d762f29f086941cec8ef1721394c3
Liu Cong <liucong@pingcap.com>
2017-11-07 22:02:59 +0800

    ch-raw: done, testing

--- ch-connector/delta/dbms/src/Magic/main.cpp
+++ ch-connector/delta/dbms/src/Magic/main.cpp
@@ -1,16 +1,34 @@
 #include <iostream>
 
-#include <Interpreters/Context.h>
-#include <Interpreters/executeQuery.h>
+#include <Common/typeid_cast.h>
+#include <Core/ColumnWithTypeAndName.h>
+#include <Columns/ColumnsNumber.h>
 #include <DataStreams/IBlockOutputStream.h>
 #include <DataStreams/copyData.h>
+#include <Interpreters/Context.h>
+#include <Interpreters/executeQuery.h>
 
 namespace Magic {
 
+// Not a effective impl, for test/dev only.
 class BlockOutputStreamPrintRows : public DB::IBlockOutputStream
 {
     void write(const DB::Block & block) override
     {
+        for (size_t i = 0; i < block.rows(); i++)
+        {
+            for (size_t j = 0; j < block.columns(); j++)
+            {
+                DB::ColumnWithTypeAndName data = block.getByPosition(i);
+
+                // TODO: support more types
+                if (data.type->getName() == "Int64") {
+                    auto column = typeid_cast<DB::ColumnInt64 *>(data.column.get());
+                    std::cout << column->getElement(j) << "\t";
+                }
+            }
+            std::cout << std::endl;
+        }
     }
 };
 
