697af1d706df4c1687db55441c9af6ac4e26f529
Liu Cong <liucong@pingcap.com>
2017-11-11 03:04:42 +0800

    Protocol comments.

--- protocol/java/src/main/java/pingcap/com/MagicProto.java
+++ protocol/java/src/main/java/pingcap/com/MagicProto.java
@@ -1,11 +1,52 @@
 package pingcap.com;
 
+/**
+ * The magic protocol.
+ *
+ * use to communicate with storage layer
+ *
+ * TODO: define Exceptions
+ */
 public class MagicProto {
+	/**
+	 * Get storage version.
+	 *
+	 * @return version string
+	 */
 	public native String version();
 
-	/*
-	 * return: handle for createBlockStream
+	/**
+	 * Execute and open a query.
+	 *
+	 * @query the query string, eg: "SELECT * FROM test".
+	 * @return token, must close later.
+	 *
+	 *  TODO: use AST query args
 	 */
-	// TODO: use AST query
-	public native String query(String query);
+	public native long query(String query);
+
+	/**
+	 * Close an opened query, no matter it finished or not.
+	 *
+	 * @token the query token.
+	 */
+	public native void close(long token);
+
+	/**
+	 * Get schema from an opened query
+	 *
+	 * @token the query token.
+	 * @return schema encoded by apache arrow
+	 */
+	public native byte[] schema(long token);
+
+	/**
+	 * Get data from an opened query
+	 *
+	 * @token create by `query`
+	 * @return
+	 *   block data (AKA: rows, or rowset, or record batch) encoded by apache arraw.
+	 *   if nil means reached the end (query execution finished and all data are fetched)
+	 */
+	public native byte[] next(long token);
 }
