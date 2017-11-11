271a3f6bb93994705716fd58d9d22589220937a9
Liu Cong <liucong@pingcap.com>
2017-11-12 01:14:39 +0800

    Encoding to arrow: schema convert, draft

--- ch-connector/delta/dbms/src/Magic/Session.h
+++ ch-connector/delta/dbms/src/Magic/Session.h
@@ -6,119 +6,83 @@
 
 namespace Magic {
 
+// TODO: handle all types
+arrow::Type dataTypeToArrowType(DataTypePtr & type)
+{
+    auto name = type->getName();
+    if (name == "Int64")
+        return arrow::Type::Int64;
+    return arrow::Type::NA;
+}
+
 // TODO: write error info to string
-class Sessions
+class Session
 {
 public:
-    class Session
+    using SchemePtr = std::shared_ptr<arrow::Schema>;
+
+    Session(const BlockIO & result_) : result(result_) {}
+
+    SchemePtr getSchema()
     {
-    public:
-        Session(const BlockIO & result_) : result(result_)
+        if (!schema)
         {
+            std::vector<std::shared_ptr<arrow::Field>> fields;
+            for (size_t i = 0; i < result.in_sample.columns(); ++i)
+            {
+                auto & column = result.in_sample.getByPosition(i);
+                auto type = arrow::DataType(dataTypeToArrowType(column.type));
+                auto field = arrow::field(column.name, type, column.type.isNullable());
+                fields.push_back(field);
+            }
+            schema = std::make_shared<arrow::Schema>(fields);
         }
+        return schema;
+    }
 
-        BufferPtr getEncodedSchema()
-        {
-            // TODO: session.result.sample => arrow encoded schema
+    BufferPtr getEncodedSchema()
+    {
+        std::shared_ptr<arrow::Buffer> serialized;
+        auto pool = arrow::default_memory_pool();
+        auto status = arrow::ipc::SerializeSchema(getSchema(), pool, &serialized);
+        if (!status.ok())
+            return result;
+        return serialized;
+    }
 
-            auto f0 = arrow::field("f0", array->type());
-            std::vector<std::shared_ptr<arrow::Field>> fields = {f0};
-            auto schema = std::make_shared<arrow::Schema>(fields);
+    BufferPtr getEncodedBlock()
+    {
+        // TODO: session.in => arrow encoded block
 
-            std::shared_ptr<arrow::Buffer> serialized;
-            auto pool = arrow::default_memory_pool();
-            auto status = arrow::ipc::SerializeSchema(schema, pool, &serialized);
+        ::arrow::Status status;
+
+        arrow::Int64Builder builder;
+        for (size_t i = 0; i < 10; i++) {
+            status = builder.Append(i);
             if (!status.ok())
                 return result;
-            return serialized;
         }
+        std::shared_ptr<arrow::Array> array;
+        status = builder.Finish(&array);
+        if (!status.ok())
+            return NULL;
 
-        BufferPtr getEncodedBlock()
-        {
-            // TODO: session.in => arrow encoded block
-
-            ::arrow::Status status;
-
-            arrow::Int64Builder builder;
-            for (size_t i = 0; i < 10; i++) {
-                status = builder.Append(i);
-                if (!status.ok())
-                    return result;
-            }
-            std::shared_ptr<arrow::Array> array;
-            status = builder.Finish(&array);
-            if (!status.ok())
-                return NULL;
-
-            arrow::RecordBatch batch(schema, 0, {array});
-            std::shared_ptr<arrow::Buffer> serialized;
-            auto pool = arrow::default_memory_pool();
-            status = arrow::ipc::SerializeRecordBatch(batch, pool, &serialized);
-            if (!status.ok())
-                return NULL;
-            return serialized;
-        }
-
-        const std::string & getErrorString()
-        {
-            return error;
-        }
-
-    private:
-        BlockIO result;
-        std::string error;
-    };
-
-    using BufferPtr = std::shared_ptr<arrow::Buffer>;
-
-    Sessions() : id_gen(10000)
-    {
+        arrow::RecordBatch batch(schema, 0, {array});
+        std::shared_ptr<arrow::Buffer> serialized;
+        auto pool = arrow::default_memory_pool();
+        status = arrow::ipc::SerializeRecordBatch(batch, pool, &serialized);
+        if (!status.ok())
+            return NULL;
+        return serialized;
     }
 
-    void init(const char * config)
+    const std::string & getErrorString()
     {
-        const char * args[] = {"--config-file", config};
-        app = new Application(args);
-    }
-
-    int64_t newSession(const char * query)
-    {
-        // TODO: lock_guard
-        auto result = DB::executeQuery(query, app.context(), false);
-        auto session = std::make_shared<Session>(result);
-        sessions[id_gen++] = session;
-    }
-
-    void closeSession(int64_t token)
-    {
-        // TODO: if .. app.cancel(...);
-        // TODO: lock_guard
-        sessions.erase(token);
-    }
-
-    Session * getSession(int64_t token)
-    {
-        // TODO: lock_guard
-        auto it = sessions.find(token);
-        return (it == sessions.end()) ? NULL : (&*it);
-    }
-
-    void close()
-    {
-        // TODO: check all sessions are closed
-        app->close();
-    }
-
-    static Sessions* global()
-    {
-        static Sessions instance;
-        return &instance;
+        return error;
     }
 
 private:
-    std::shared_ptr<DB::Application> app;
-    std::unordered_map<int64_t, std::shared_ptr<Session>> sessions;
-    long id_gen;
+    BlockIO result;
+    std::string error;
 };
-
 }
