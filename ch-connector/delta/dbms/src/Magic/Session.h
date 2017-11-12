d5025afb0f6c025b898f54cb3f85dc2eb80124a2
Liu Cong <liucong@pingcap.com>
2017-11-12 13:20:08 +0800

    Session: impl done

--- ch-connector/delta/dbms/src/Magic/Session.h
+++ ch-connector/delta/dbms/src/Magic/Session.h
@@ -7,65 +7,36 @@
 namespace Magic {
 
 
-// TODO: handle all types
-arrow::Type dataTypeToArrowType(DB::DataTypePtr & type)
-{
-    auto name = type->getName();
-    if (name == "Int64")
-        return arrow::Type::Int64;
-    return arrow::Type::NA;
-}
-
-
-// TODO: handle all types
-// TODO: faster copy
-std::shared_ptr<Array> columnToArrowArray(DB::DataTypePtr & type, DB::ColumnPtr & column, size_t rows)
-{
-    auto name = type->getName();
-    std::shared_ptr<arrow::Array> array;
-
-    if (name == "Int64")
-    {
-        const auto & data = typeid_cast<DB::ColumnInt64 &>(*column);
-        arrow::Int64Builder builder;
-        for (size_t i = 0; i < rows; ++i)
-        {
-            status = builder.Append(data.getElement(i));
-            if (!status.ok())
-                return NULL;
-        }
-        status = builder.Finish(&array);
-        if (!status.ok())
-            return NULL;
-    }
-
-    return array;
-}
-
-
-// TODO: write error info to string
 class Session
 {
 public:
-    using SchemePtr = std::shared_ptr<arrow::Schema>;
-    using Block = arrow::RecordBatch;
+    using SchemaPtr = std::shared_ptr<arrow::Schema>;
+    using BlockPtr = std::shared_ptr<arrow::RecordBatch>;
+    using BufferPtr = std::shared_ptr<arrow::Buffer>;
 
     Session(const DB::BlockIO & result_) : result(result_)
     {
-        result.in.readPrefix();
-
-        std::vector<std::shared_ptr<arrow::Field>> fields;
-        for (size_t i = 0; i < result.in_sample.columns(); ++i)
+        try
         {
-            auto & column = result.in_sample.getByPosition(i);
-            auto type = arrow::DataType(dataTypeToArrowType(column.type));
-            auto field = arrow::field(column.name, type, column.type.isNullable());
-            fields.push_back(field);
+            result.in->readPrefix();
+
+            std::vector<std::shared_ptr<arrow::Field>> fields;
+            for (size_t i = 0; i < result.in_sample.columns(); ++i)
+            {
+                auto & column = result.in_sample.getByPosition(i);
+                auto type = dataTypeToArrowType(column.type);
+                auto field = arrow::field(column.name, type, column.type->isNullable());
+                fields.push_back(field);
+            }
+            schema = std::make_shared<arrow::Schema>(fields);
         }
-        schema = std::make_shared<arrow::Schema>(fields);
+        catch (const DB::Exception & e)
+        {
+            error = e.displayText();
+        }
     }
 
-    SchemePtr getSchema()
+    SchemaPtr getSchema()
     {
         return schema;
     }
@@ -74,37 +45,55 @@ public:
     {
         std::shared_ptr<arrow::Buffer> serialized;
         auto pool = arrow::default_memory_pool();
-        auto status = arrow::ipc::SerializeSchema(schema, pool, &serialized);
+        auto status = arrow::ipc::SerializeSchema(*schema, pool, &serialized);
         if (!status.ok())
-            return result;
+        {
+            error = "arrow::ipc::SerializeSchema " + status.ToString();
+            return NULL;
+        }
         return serialized;
     }
 
-    Block getBlock()
+    BlockPtr getBlock()
     {
-        DB::Block block = result.in.read();
-        std::vector<std::shared_ptr<Array>> arrays;
-        ::arrow::Status status;
-
-        for (size_t i = 0; i < block.columns(); ++i)
+        try
         {
-            auto & column = result.in_sample.getByPosition(i);
-            auto array = columnToArrowArray(column.type, column.column, block.rows());
-            arrays.push_back(array);
+            DB::Block block = result.in->read();
+            if (!block)
+                return NULL;
+            std::vector<std::shared_ptr<arrow::Array>> arrays;
+            arrow::Status status;
+
+            for (size_t i = 0; i < block.columns(); ++i)
+            {
+                auto & column = result.in_sample.getByPosition(i);
+                auto array = columnToArrowArray(column.type, column.column, block.rows());
+                arrays.push_back(array);
+            }
+
+            return std::make_shared<arrow::RecordBatch>(schema, block.rows(), arrays);
+        }
+        catch (const DB::Exception & e)
+        {
+            error = e.displayText();
         }
 
-        arrow::RecordBatch batch(schema, block.rows(), arrays);
-        return batch;
+        return NULL;
     }
 
     BufferPtr getEncodedBlock()
     {
-        auto batch = getBlock();
+        auto block = getBlock();
+        if (!block)
+            return NULL;
         std::shared_ptr<arrow::Buffer> serialized;
         auto pool = arrow::default_memory_pool();
-        status = arrow::ipc::SerializeRecordBatch(batch, pool, &serialized);
+        auto status = arrow::ipc::SerializeRecordBatch(*block, pool, &serialized);
         if (!status.ok())
+        {
+            error = "arrow::ipc::SerializeRecordBatch " + status.ToString();
             return NULL;
+        }
         return serialized;
     }
 
@@ -114,8 +103,51 @@ public:
     }
 
 private:
-    BlockIO result;
+    // TODO: faster copy
+    // TODO: handle all types
+    std::shared_ptr<arrow::Array> columnToArrowArray(DB::DataTypePtr & type, DB::ColumnPtr & column, size_t rows)
+    {
+        auto name = type->getName();
+        std::shared_ptr<arrow::Array> array;
+        arrow::Status status;
+
+        if (name == "Int64")
+        {
+            const auto & data = typeid_cast<DB::ColumnInt64 &>(*column);
+            arrow::Int64Builder builder;
+            for (size_t i = 0; i < rows; ++i)
+            {
+                status = builder.Append(data.getElement(i));
+                if (!status.ok())
+                {
+                    error = "arrow::Int64Builder.Append " + status.ToString();
+                    return NULL;
+                }
+            }
+            status = builder.Finish(&array);
+            if (!status.ok())
+            {
+                error = "arrow::Int64Builder.Finish" + status.ToString();
+                return NULL;
+            }
+        }
+
+        return array;
+    }
+
+    // TODO: handle all types
+    static std::shared_ptr<arrow::DataType> dataTypeToArrowType(DB::DataTypePtr & type)
+    {
+        auto name = type->getName();
+        if (name == "Int64")
+            return arrow::int64();
+        return arrow::null();
+    }
+
+private:
+    DB::BlockIO result;
     std::string error;
+    SchemaPtr schema;
 };
 
 
