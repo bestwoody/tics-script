2766ec1f831c82ef3426116b5d5b721f190d3a78
Liu Cong <liucong@pingcap.com>
2017-11-12 20:42:19 +0800

    Work around: setup DYLD_LIBRARY_PATH when run ch-raw

--- ch-connector/ch-raw-run.sh
+++ ch-connector/ch-raw-run.sh
@@ -1,1 +1,1 @@
-build/dbms/src/Magic/ch-raw --config-file "running/config/config.xml" --query "SELECT * FROM test"
+DYLD_LIBRARY_PATH="" build/dbms/src/Magic/ch-raw --config-file "running/config/config.xml" --query "SELECT * FROM test"
