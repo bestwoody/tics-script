4e743801503ce1ab8db4871e3772eeaf7d92dff4
Liu Cong <liucong@pingcap.com>
2017-11-14 16:40:19 +0800

    Support piles of numberic types

--- ch-connector/delta/dbms/src/Magic/BlockOutputStreamPrintRows.h
+++ ch-connector/delta/dbms/src/Magic/BlockOutputStreamPrintRows.h
@@ -21,17 +21,63 @@ public:
             for (size_t j = 0; j < block.columns(); j++)
             {
                 DB::ColumnWithTypeAndName data = block.getByPosition(j);
+                auto name = data.type->getName();
 
                 // TODO: support more types
-                if (data.type->getName() == "Int64")
+                if (name == "String")
+                {
+                    const auto & column = typeid_cast<DB::ColumnString &>(*data.column);
+                    print(i, j, column.getDataAt(i).toString());
+                }
+                else if (name == "Int8")
+                {
+                    const auto & column = typeid_cast<DB::ColumnInt8 &>(*data.column);
+                    print(i, j, column.getElement(i));
+                }
+                else if (name == "Int16")
+                {
+                    const auto & column = typeid_cast<DB::ColumnInt16 &>(*data.column);
+                    print(i, j, column.getElement(i));
+                }
+                else if (name == "Int32")
+                {
+                    const auto & column = typeid_cast<DB::ColumnInt32 &>(*data.column);
+                    print(i, j, column.getElement(i));
+                }
+                else if (name == "Int64")
                 {
                     const auto & column = typeid_cast<DB::ColumnInt64 &>(*data.column);
                     print(i, j, column.getElement(i));
                 }
-                else if (data.type->getName() == "String")
+                else if (name == "UInt8")
                 {
-                    const auto & column = typeid_cast<DB::ColumnString &>(*data.column);
-                    print(i, j, column.getDataAt(i).toString());
+                    const auto & column = typeid_cast<DB::ColumnUInt8 &>(*data.column);
+                    print(i, j, column.getElement(i));
+                }
+                else if (name == "UInt16")
+                {
+                    const auto & column = typeid_cast<DB::ColumnUInt16 &>(*data.column);
+                    print(i, j, column.getElement(i));
+                }
+                else if (name == "UInt32")
+                {
+                    const auto & column = typeid_cast<DB::ColumnUInt32 &>(*data.column);
+                    print(i, j, column.getElement(i));
+                }
+                else if (name == "UInt64")
+                {
+                    const auto & column = typeid_cast<DB::ColumnUInt64 &>(*data.column);
+                    print(i, j, column.getElement(i));
+                }
+                else if (name == "Float32")
+                {
+                    const auto & column = typeid_cast<DB::ColumnFloat32 &>(*data.column);
+                    print(i, j, column.getElement(i));
+                }
+                else if (name == "Float64")
+                {
+                    const auto & column = typeid_cast<DB::ColumnFloat64 &>(*data.column);
+                    print(i, j, column.getElement(i));
                 }
             }
             writer << std::endl;
--- docs/supported-types.md
+++ docs/supported-types.md
@@ -1,8 +1,17 @@
 # Supported types
 
 ## Current
-* Int64
 * String
+* Int8
+* Int16
+* Int32
+* Int64
+* UInt8
+* UInt16
+* UInt32
+* UInt64
+* Float32
+* Float64
 
 ## TPCH
 * Int
