b7a6e3f16cd3ad08b52fddad25806f7ca9d9ea42
Liu Cong <liucong@pingcap.com>
2017-11-15 23:28:45 +0800

    Wrap libch as: java class Magic

--- spark-connector/java/src/main/java/pingcap/com/App.java
+++ spark-connector/java/src/main/java/pingcap/com/App.java
@@ -1,27 +1,5 @@
 package pingcap.com;
 
-import static java.util.Arrays.asList;
-
-import java.util.List;
-import java.util.ArrayList;
-import java.io.ByteArrayInputStream;
-import java.nio.channels.Channels;
-
-import io.netty.buffer.ArrowBuf;
-import org.apache.arrow.memory.BufferAllocator;
-import org.apache.arrow.memory.RootAllocator;
-import org.apache.arrow.vector.file.ReadChannel;
-import org.apache.arrow.vector.stream.MessageSerializer;
-import org.apache.arrow.vector.schema.ArrowRecordBatch;
-import org.apache.arrow.vector.types.pojo.Field;
-import org.apache.arrow.vector.types.pojo.FieldType;
-import org.apache.arrow.vector.types.pojo.Schema;
-import org.apache.arrow.vector.types.pojo.ArrowType;
-import org.apache.arrow.vector.types.pojo.ArrowType.Int;
-import org.apache.arrow.vector.VectorSchemaRoot;
-import org.apache.arrow.vector.VectorLoader;
-import org.apache.arrow.vector.NullableBigIntVector;
-
 public class App {
 	public static void main(String[] args) throws Exception {
 		if (args.length < 1) {
@@ -29,10 +7,7 @@ public class App {
 			System.exit(-1);
 		}
 
-		System.loadLibrary("ch");
-
-		MagicProto magic= new MagicProto();
-
+		Magic magic = new Magic();
 		String cmd = args[0];
 
 		if (cmd.equals("version")) {
@@ -49,33 +24,14 @@ public class App {
 			String query = args[2];
 
 			System.out.println("[" + query + "]");
+			magic.init(path);
 
-			MagicProto.InitResult initResult = magic.init(path);
-			if (initResult.error != null) {
-				System.out.println("init failed: " + initResult.error);
-				System.exit(-1);
-			}
-
-			MagicProto.QueryResult queryResult = magic.query(query);
-			if (queryResult.error != null) {
-				System.out.println("query failed: " + queryResult.error);
-				System.exit(-1);
-			}
-
-			BlockStream stream = new BlockStream(magic, queryResult.token);
+			Magic.Query result = magic.query(query);
+			BlockStream stream = new BlockStream(result);
 			stream.dump();
-
-			MagicProto.CloseResult closeResult = magic.close(queryResult.token);
-			if (closeResult.error != null) {
-				System.out.println("close failed: " + closeResult.error);
-				System.exit(-1);
-			}
-
-			MagicProto.FinishResult finishResult = magic.finish();
-			if (finishResult.error != null) {
-				System.out.println("finish failed: " + finishResult.error);
-				System.exit(-1);
-			}
+			result.close();
 		}
+
+		magic.close();
 	}
 }
--- spark-connector/java/src/main/java/pingcap/com/BlockStream.java
+++ spark-connector/java/src/main/java/pingcap/com/BlockStream.java
@@ -1,82 +1,23 @@
 package pingcap.com;
 
-import static java.util.Arrays.asList;
-
 import java.util.List;
-import java.util.ArrayList;
-import java.io.ByteArrayInputStream;
-import java.nio.channels.Channels;
 
-import io.netty.buffer.ArrowBuf;
-import org.apache.arrow.memory.BufferAllocator;
-import org.apache.arrow.memory.RootAllocator;
-import org.apache.arrow.vector.file.ReadChannel;
-import org.apache.arrow.vector.stream.MessageSerializer;
-import org.apache.arrow.vector.schema.ArrowRecordBatch;
 import org.apache.arrow.vector.types.pojo.Field;
-import org.apache.arrow.vector.types.pojo.FieldType;
 import org.apache.arrow.vector.types.pojo.Schema;
-import org.apache.arrow.vector.types.pojo.ArrowType;
-import org.apache.arrow.vector.types.pojo.ArrowType.Int;
 import org.apache.arrow.vector.VectorSchemaRoot;
-import org.apache.arrow.vector.VectorLoader;
 import org.apache.arrow.vector.FieldVector;
 import org.apache.arrow.vector.ValueVector;
 
-import org.apache.arrow.vector.BigIntVector;
-import org.apache.arrow.vector.VarCharVector;
-
-import org.apache.arrow.vector.NullableBigIntVector;
-import org.apache.arrow.vector.NullableVarCharVector;
-
-import org.apache.arrow.vector.types.pojo.ArrowType;
-
 public class BlockStream {
-	private MagicProto magic;
-	private long token;
-	private BufferAllocator alloc;
-	private Schema schema;
+	private Magic.Query query;
 
-	public BlockStream(MagicProto magic, long token) {
-		this.magic = magic;
-		this.token = token;
-		this.alloc = new RootAllocator(Long.MAX_VALUE);
-	}
-
-	private Schema schema() throws Exception {
-		if (schema == null) {
-			// TODO: maybe better: Schema::deserialize(ByteBuffer buffer)
-			byte[] result = magic.schema(token);
-			ByteArrayInputStream in = new ByteArrayInputStream(result);
-			ReadChannel channel = new ReadChannel(Channels.newChannel(in));
-			schema = MessageSerializer.deserializeSchema(channel);
-		}
-		return schema;
-	}
-
-	public VectorSchemaRoot next() throws Exception {
-		byte[] result = magic.next(token);
-		if (result == null) {
-			return null;
-		}
-
-		ByteArrayInputStream in = new ByteArrayInputStream(result);
-		ReadChannel channel = new ReadChannel(Channels.newChannel(in));
-		ArrowRecordBatch batch = (ArrowRecordBatch)MessageSerializer.deserializeMessageBatch(channel, alloc);
-
-		VectorSchemaRoot root = VectorSchemaRoot.create(schema, alloc);
-		VectorLoader loader = new VectorLoader(root);
-		loader.load(batch);
-		return root;
+	public BlockStream(Magic.Query query) {
+		this.query = query;
 	}
 
 	// Just for test
 	public void dump() throws Exception {
-		Schema schema = schema();
-		if (schema == null) {
-			System.out.println("get schema failed: " + magic.error(token));
-			return;
-		}
+		Schema schema = query.schema();
 		List<Field> fields = schema.getFields();
 		int i = 0;
 		for (Field field: fields) {
@@ -86,12 +27,8 @@ public class BlockStream {
 		}
 
 		while (true) {
-			VectorSchemaRoot block = next();
+			VectorSchemaRoot block = query.next();
 			if (block == null) {
-				String error = magic.error(token);
-				if (error != null) {
-					System.out.println("get block failed: " + error);
-				}
 				break;
 			}
 			System.out.println("======");
