f2698c1ebd3d35edca7cf6474565102708db27c8
Liu Cong <liucong@pingcap.com>
2017-11-20 22:53:01 +0800

    Fix: ch-raw print int8/uint8 as char

--- ch-connector/delta/dbms/src/Magic/BlockOutputStreamPrintRows.h
+++ ch-connector/delta/dbms/src/Magic/BlockOutputStreamPrintRows.h
@@ -8,82 +8,90 @@
 #include <DataStreams/IBlockOutputStream.h>
 #include <DataStreams/copyData.h>
 
+
+namespace DB
+{
+
 // Not a effective impl, for test/dev only.
-class BlockOutputStreamPrintRows : public DB::IBlockOutputStream
+class BlockOutputStreamPrintRows : public IBlockOutputStream
 {
 public:
     BlockOutputStreamPrintRows(std::ostream & writer_) : writer(writer_), block_index(0) {}
 
-    void write(const DB::Block & block) override
+    void write(const Block & block) override
     {
         for (size_t i = 0; i < block.rows(); i++)
         {
             for (size_t j = 0; j < block.columns(); j++)
             {
-                DB::ColumnWithTypeAndName data = block.getByPosition(j);
+                ColumnWithTypeAndName data = block.getByPosition(j);
                 auto name = data.type->getName();
 
                 // TODO: support all types
                 // TODO: check by id, not by name
                 if (name == "String")
                 {
-                    const auto & column = typeid_cast<DB::ColumnString &>(*data.column);
+                    const auto & column = typeid_cast<ColumnString &>(*data.column);
                     print(i, j, column.getDataAt(i).toString());
                 }
                 else if (name == "DateTime")
                 {
-                    const auto & column = typeid_cast<DB::ColumnUInt32 &>(*data.column);
-                    print(i, j, column.getElement(i));
+                    const auto & column = typeid_cast<ColumnUInt32 &>(*data.column);
+                    print(i, j, (int64_t)column.getElement(i));
                 }
                 else if (name == "Int8")
                 {
-                    const auto & column = typeid_cast<DB::ColumnInt8 &>(*data.column);
-                    print(i, j, column.getElement(i));
+                    const auto & column = typeid_cast<ColumnInt8 &>(*data.column);
+                    print(i, j, (int64_t)column.getElement(i));
                 }
                 else if (name == "Int16")
                 {
-                    const auto & column = typeid_cast<DB::ColumnInt16 &>(*data.column);
-                    print(i, j, column.getElement(i));
+                    const auto & column = typeid_cast<ColumnInt16 &>(*data.column);
+                    print(i, j, (int64_t)column.getElement(i));
                 }
                 else if (name == "Int32")
                 {
-                    const auto & column = typeid_cast<DB::ColumnInt32 &>(*data.column);
-                    print(i, j, column.getElement(i));
+                    const auto & column = typeid_cast<ColumnInt32 &>(*data.column);
+                    print(i, j, (int64_t)column.getElement(i));
                 }
                 else if (name == "Int64")
                 {
-                    const auto & column = typeid_cast<DB::ColumnInt64 &>(*data.column);
-                    print(i, j, column.getElement(i));
+                    const auto & column = typeid_cast<ColumnInt64 &>(*data.column);
+                    print(i, j, (int64_t)column.getElement(i));
                 }
                 else if (name == "UInt8")
                 {
-                    const auto & column = typeid_cast<DB::ColumnUInt8 &>(*data.column);
-                    print(i, j, column.getElement(i));
+                    const auto & column = typeid_cast<ColumnUInt8 &>(*data.column);
+                    print(i, j, (int64_t)column.getElement(i));
                 }
                 else if (name == "UInt16")
                 {
-                    const auto & column = typeid_cast<DB::ColumnUInt16 &>(*data.column);
-                    print(i, j, column.getElement(i));
+                    const auto & column = typeid_cast<ColumnUInt16 &>(*data.column);
+                    print(i, j, (int64_t)column.getElement(i));
                 }
                 else if (name == "UInt32")
                 {
-                    const auto & column = typeid_cast<DB::ColumnUInt32 &>(*data.column);
-                    print(i, j, column.getElement(i));
+                    const auto & column = typeid_cast<ColumnUInt32 &>(*data.column);
+                    print(i, j, (int64_t)column.getElement(i));
                 }
                 else if (name == "UInt64")
                 {
-                    const auto & column = typeid_cast<DB::ColumnUInt64 &>(*data.column);
-                    print(i, j, column.getElement(i));
+                    const auto & column = typeid_cast<ColumnUInt64 &>(*data.column);
+                    print(i, j, (uint64_t)column.getElement(i));
                 }
                 else if (name == "Float32")
                 {
-                    const auto & column = typeid_cast<DB::ColumnFloat32 &>(*data.column);
+                    const auto & column = typeid_cast<ColumnFloat32 &>(*data.column);
                     print(i, j, column.getElement(i));
                 }
                 else if (name == "Float64")
                 {
-                    const auto & column = typeid_cast<DB::ColumnFloat64 &>(*data.column);
+                    const auto & column = typeid_cast<ColumnFloat64 &>(*data.column);
                     print(i, j, column.getElement(i));
+                }
+                else
+                {
+                    throw Exception(std::string("unknown type name: ") + name);
                 }
             }
             writer << std::endl;
@@ -104,3 +112,5 @@ private:
     std::ostream & writer;
     size_t block_index;
 };
+
+}
