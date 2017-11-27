da66bc23eeeb23bc28a2a02cdff0606e5aa92af9
Liu Cong <liucong@pingcap.com>
2017-11-27 21:03:21 +0800

    CHContext demo draft

---
+++ spark-connector/ch-spark/src/main/scala/org/apache/spark/sql/CHContext.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2017 PingCAP, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql
+
+import org.apache.spark.SparkConf
+import org.apache.spark.internal.Logging
+
+import org.apache.spark.sql.ch.Relation
+
+class CHContext (val session: SparkSession) extends Serializable with Logging {
+  val sqlContext: SQLContext = session.sqlContext
+  val conf: SparkConf = session.sparkContext.conf
+
+  // session.experimental.extraStrategies ++= Seq(new Strategy(sqlContext))
+
+  def chMapDatabase(): Unit = {
+    val name = "test"
+    val rel = new Relation(name)
+    sqlContext.baseRelationToDataFrame(rel).createTempView(name)
+  }
+}
