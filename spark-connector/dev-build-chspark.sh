a2c64557e50908fddfb9c7cdb62b05242f6c378a
Liu Cong <liucong@pingcap.com>
2017-11-28 02:13:44 +0800

    Small changes

--- spark-connector/dev-build-chspark.sh
+++ spark-connector/dev-build-chspark.sh
@@ -1,17 +1,17 @@
 set -eu
 
-#cd chspark
-#mvn package
+old=`pwd`
+cd chspark
+mvn package
+cd "$old"
 
 spark_jp=""
 for jp in spark/assembly/target/scala-*/jars; do
 	spark_jp="$jp"
 	break
 done
-
-if [ -z "$spark_jp" ]; then
+if [ ! -d "$spark_jp" ]; then
 	echo "spark jars path not found" >&2
 	exit 1
 fi
-
 cp "chspark/target/chspark-0.1.0-SNAPSHOT-jar-with-dependencies.jar" "$spark_jp/"
