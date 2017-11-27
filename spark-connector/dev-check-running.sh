ef3ebbd02c616b42d031c6510db3216b4b9863ff
Liu Cong <liucong@pingcap.com>
2017-11-28 02:22:34 +0800

    Dev scripts

--- spark-connector/dev-check-running.sh
+++ spark-connector/dev-check-running.sh
@@ -1,4 +1,7 @@
 master_check=`ps -ef | grep org.apache.spark.deploy.master.Master | grep -v grep | wc -l | awk '{print $1}'`
 echo "$master_check master(s) is running"
+
 slave_check=`ps -ef | grep org.apache.spark.deploy.worker.Worker | grep -v grep | wc -l | awk '{print $1}'`
+slave_in_master_check=`curl -s http://127.0.0.1:8080 | grep ' Workers (' | awk -F '(' '{print $2}' | awk -F ')' '{print $1}'`
 echo "$slave_check slave(s) is running"
+echo "$slave_in_master_check slave(s) in master"
