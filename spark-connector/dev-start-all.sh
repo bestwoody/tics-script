d930abc172e51d355022def04b446a14343963b7
Liu Cong <liucong@pingcap.com>
2017-11-27 22:44:23 +0800

    Dev scripts: grace error check

--- spark-connector/dev-start-all.sh
+++ spark-connector/dev-start-all.sh
@@ -1,8 +1,23 @@
 ip=`ifconfig | grep -i mask | grep broadcast | grep inet | awk '{print $2}'`
-
-# TODO: check ip
+ip_check="`echo $ip | wc -l | awk '{print $1}'`"
+if [ "$ip_check" != "1" ]; then
+	echo "get local ip failed: $ip_check" >&2
+	exit 1
+fi
 
 spark/sbin/start-master.sh
-spark/sbin/start-slave.sh $ip:7077
+master_check=`ps -ef | grep org.apache.spark.deploy.master.Master | grep -v grep | wc -l | awk '{print $1}'`
+if [ "$master_check" != "1" ]; then
+	echo "launch master failed: $master_check" >&2
+	exit 1
+fi
 
-# TODO: check alive
+spark/sbin/start-slave.sh $ip:7077
+slave_check=`ps -ef | grep org.apache.spark.deploy.worker.Worker | grep -v grep | wc -l | awk '{print $1}'`
+if [ "$slave_check" != "1" ]; then
+	echo "launch slave failed: $slave_check" >&2
+	exit 1
+fi
+
+echo "" >&2
+echo "OK" >&2
--- spark-connector/dev-stop-all.sh
+++ spark-connector/dev-stop-all.sh
@@ -1,4 +1,16 @@
 spark/sbin/stop-master.sh
 spark/sbin/stop-slave.sh
 
-# TODO: check all grace exit
+sleep 0.5
+
+master_check=`ps -ef | grep org.apache.spark.deploy.master.Master | grep -v grep | wc -l | awk '{print $1}'`
+if [ "$master_check" != "0" ]; then
+	echo "stop master failed: $master_check" >&2
+fi
+master_check=`ps -ef | grep org.apache.spark.deploy.master.Master | grep -v grep | wc -l | awk '{print $1}'`
+if [ "$master_check" != "0" ]; then
+	echo "stop master failed: $master_check" >&2
+fi
+
+echo "" >&2
+echo "OK" >&2
