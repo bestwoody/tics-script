0c00a2f9eec741c0ceae3cee9047144d2507fcb1
Liu Cong <liucong@pingcap.com>
2017-11-29 01:09:14 +0800

    RDD simple impl

---
+++ spark-connector/chspark/src/main/scala/org/apache/spark/sql/ch/CHRDD.scala
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2017 PingCAP, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.ch
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.{Partition, TaskContext}
+import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.unsafe.types.UTF8String
+
+
+class CHRDD(@transient private val sparkSession: SparkSession)
+  extends RDD[Row](sparkSession.sparkContext, Nil) {
+
+  override def compute(split: Partition, context: TaskContext): Iterator[Row] = new Iterator[Row] {
+    val iterator = Iterator("aaa", "bbb", "ccc")
+    override def hasNext: Boolean = iterator.hasNext
+    override def next(): Row = Row.fromSeq(Seq(iterator.next))
+  }
+
+  override protected def getPartitions: Array[Partition] = {
+    Array(new CHPartition(0))
+  }
+}
