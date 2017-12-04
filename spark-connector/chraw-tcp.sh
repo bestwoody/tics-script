298593872956fa5e9b73e93cf32091714e61e329
Liu Cong <liucong@pingcap.com>
2017-12-05 01:48:18 +0800

    Tidy up testing env and scripts

--- spark-connector/chraw-tcp.sh
+++ spark-connector/chraw-tcp.sh
@@ -4,6 +4,11 @@ port="9006"
 
 set -eu
 
+if [ -z "$query" ]; then
+	echo "usage: <bin> $query [ch-host] [ch-port]" >&2
+	exit 1
+fi
+
 java \
 	-cp ch-sdk/target/*:ch-sdk/target/lib/* \
 	org.apache.spark.sql.ch/CHRaw \
