9847d9479172f647fd49d9de7875e7bdff63ebed
Liu Cong <liucong@pingcap.com>
2017-12-14 08:59:45 +0800

    Add spark config file to spec memory settings.

--- benchmark/spark-shell.sh
+++ benchmark/spark-shell.sh
@@ -1,2 +1,9 @@
 source _env.sh
+
+cp "$repo_dir/spark-connector/conf/spark-defaults.conf" "$repo_dir/spark-connector/spark/conf/"
+if [ "$?" != "0" ]; then
+	echo "Copy config file to spark failed." >&2
+	exit 1
+fi
+
 $repo_dir/spark-connector/spark/bin/spark-shell $@
