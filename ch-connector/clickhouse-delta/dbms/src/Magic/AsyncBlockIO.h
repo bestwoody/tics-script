ce26c65896a7ef7c986fa71367e3ee0dea90b7ac
Liu Cong <liucong@pingcap.com>
2017-12-20 04:37:35 +0800

    BlockIO tool: graceful close BlockInputstream

---
+++ ch-connector/clickhouse-delta/dbms/src/Magic/AsyncBlockIO.h
@@ -0,0 +1,106 @@
+#pragma once
+
+#include <DataStreams/BlockIO.h>
+#include <DataStreams/AsynchronousBlockInputStream.h>
+
+namespace Magic
+{
+
+class AsyncBlockIO
+{
+public:
+    enum { UnknownSize = size_t(-1) };
+
+    AsyncBlockIO(DB::BlockIO & origin_) : origin(origin_), input(origin.in), closed(false),
+        block_count(0), batch_count(0),
+        interactive_delay_ms(300), batch_size(4)
+    {
+        std::lock_guard<std::mutex> lock(mutex);
+        input.readPrefix();
+    }
+
+    DB::Block sample()
+    {
+        std::lock_guard<std::mutex> lock(mutex);
+        return origin.in_sample;
+    }
+
+    void read(std::vector<DB::Block> & dest, bool & closed)
+    {
+        std::lock_guard<std::mutex> lock(mutex);
+
+        closed = this->closed;
+        if (closed)
+            return;
+
+        while (dest.size() != batch_size)
+        {
+            DB::Block block;
+            while (true)
+            {
+                if (input.poll(interactive_delay_ms))
+                {
+                    block = input.read();
+                    break;
+                }
+            }
+
+            if (!block)
+            {
+                closed = true;
+                close();
+                break;
+            }
+            else
+            {
+                block_count += 1;
+                dest.push_back(block);
+            }
+        }
+
+        if (dest.size())
+            batch_count += 1;
+    }
+
+    void cancal(bool exception = false)
+    {
+        std::lock_guard<std::mutex> lock(mutex);
+        close(exception);
+    }
+
+    size_t size()
+    {
+        std::lock_guard<std::mutex> lock(mutex);
+        if (closed)
+            return batch_count;
+        else
+            return UnknownSize;
+    }
+
+private:
+    void close(bool exception = false)
+    {
+        if (closed)
+            return;
+        input.cancel();
+        input.readSuffix();
+        if (exception)
+            origin.onException();
+        origin.onFinish();
+        closed = true;
+    }
+
+private:
+    std::mutex mutex;
+
+    DB::BlockIO & origin;
+    DB::AsynchronousBlockInputStream input;
+    bool closed;
+    size_t block_count;
+    size_t batch_count;
+
+    size_t interactive_delay_ms;
+    size_t batch_size;
+};
+
+}
