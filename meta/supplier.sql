c97e03004c372c12622543499a58d580f46e5231
xiao liang liang <xiaoliangliang@pingcap.com>
2017-12-26 00:31:10 +0800

    delete meta

--- meta/supplier.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS supplier (
-	S_SUPPKEY     UInt8,
-	S_NAME        String,
-	S_ADDRESS     String,
-	S_NATIONKEY   UInt8,
-	S_PHONE       String,
-	S_ACCTBAL     Float64,
-	S_COMMENT     String
-	)ENGINE = Log;
