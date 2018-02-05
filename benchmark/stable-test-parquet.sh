29a91142a68d8dd68c4236f2a5f5607146ea0242
Liu Cong <liucong@pingcap.com>
2018-02-05 22:48:57 +0800

    Fix: no ignore queries in parquet test.

--- benchmark/stable-test-parquet.sh
+++ benchmark/stable-test-parquet.sh
@@ -9,16 +9,9 @@ rm -f "$log"
 
 for (( i = 0; i < $count; i++ )); do
 	for (( j = 1; j <= 21; j++ )); do
-
-		if [ $j -eq 15 ] || [ $j -eq 17 ] || [ $j -eq 18 ]  || [ $j -eq 20 ] || [ $j -eq 21 ]; then
-			continue
-		fi
-
 		# ./clear-page-cache.sh
-
 		echo "## Running tpch query #"$j", parquet" >>$log
 		./tpch-parquet-q.sh $j >>$log 2>&1
 		echo >>$log
-
 	done
 done
