3d4259f23478318e7be4a3bb09efcf37897fedc5
Liu Cong <liucong@pingcap.com>
2018-02-09 22:45:58 +0800

    Add tcp transfer interface benchmark script.

---
+++ benchmark/chraw-bench.sh
@@ -0,0 +1,50 @@
+function run()
+{
+    cd ../spark-connector && ./chraw-scala.sh "select * from lineitem" "$1" "$2" "$3"
+}
+
+run 1 4 2
+run 2 4 1
+run 2 4 2
+run 4 4 1
+run 1 4 4
+run 4 4 2
+run 2 4 4
+run 4 4 4
+run 8 4 1
+run 1 4 8
+run 8 4 2
+run 2 4 8
+run 8 4 4
+run 4 4 8
+run 8 4 8
+run 16 4 1
+run 24 4 1
+run 32 4 1
+run 64 4 1
+run 128 4 1
+run 16 4 2
+run 24 4 2
+run 32 4 2
+run 64 4 2
+run 128 4 2
+run 16 4 4
+run 24 4 4
+run 32 4 4
+run 64 4 4
+run 128 4 4
+run 16 4 8
+run 24 4 8
+run 32 4 8
+run 64 4 8
+run 128 4 8
+run 16 4 16
+run 24 4 16
+run 32 4 16
+run 64 4 16
+run 128 4 16
+run 16 4 32
+run 24 4 32
+run 32 4 32
+run 64 4 32
+run 128 4 32
