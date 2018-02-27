c79c0c323717873d5b6d961943c4157e34e84801
Liu Cong <innerr@gmail.com>
2018-02-28 04:37:44 +0800

    Auto generate test file by table parts visualization. (#184)
    * Dedup testcase visualization.
    
    * Visualized testcases.
    
    * Visualized testcases, not finished yet.
    
    * Small changes: rename.
    
    * WIP: visual parts merging test.
    
    * WIP: visual parts merging test: gen diff orders tests.
    
    * WIP: visual parts merging test: gen test files.
    
    * Auto generate test file by table parts visualization.
    
    * Auto generate test file by table parts visualization.
    
    * Test script: add more error check

--- ch-connector/mutable-test.sh
+++ ch-connector/mutable-test.sh
@@ -17,11 +17,40 @@ function run_dir()
 	local dbc="$1"
 	local path="$2"
 
-	find "$path" -name "*.test" -type f | while read file; do
+	find "$path" -name "*.visual" -depth 1 -type f | sort | while read file; do
+		if [ -f "$file" ]; then
+			python mutable-test-visual.py "$file"
+		fi
+		if [ $? != 0 ]; then
+			echo "Generate test files failed: $file" >&2
+			exit
+		fi
+	done
+
+	if [ $? != 0 ]; then
+		echo "Generate test files failed" >&2
+		exit 1
+	fi
+
+	find "$path" -name "*.test" -depth 1 -type f | sort | while read file; do
 		if [ -f "$file" ]; then
 			run_file "$dbc" "$file"
 		fi
 	done
+
+	if [ $? != 0 ]; then
+		exit 1
+	fi
+
+	find "$path" -depth 1 -type d | sort -r | while read dir; do
+		if [ -d "$dir" ]; then
+			run_dir "$dbc" "$dir"
+		fi
+	done
+
+	if [ $? != 0 ]; then
+		exit 1
+	fi
 }
 
 function run_path()
