7ed0a77d165f95b0e74a7aeccc84b72e14222223
Liu Cong <liucong@pingcap.com>
2018-03-02 00:05:40 +0800

    Fixing duplicated key in the same block when executing 'select'.

--- ch-connector/clickhouse-patch/dbms/src/Storages/MutableSupport.cpp
+++ ch-connector/clickhouse-patch/dbms/src/Storages/MutableSupport.cpp
@@ -4,8 +4,8 @@
 namespace DB
 {
 
-const std::string MutableSupport::mutable_storage_name = "MutableMergeTree";
-const std::string MutableSupport::mutable_version_column_name = "_INTERNAL_VERSION";
-const std::string MutableSupport::mutable_delmark_column_name = "_INTERNAL_DELMARK";
+const std::string MutableSupport::storage_name = "MutableMergeTree";
+const std::string MutableSupport::version_column_name = "_INTERNAL_VERSION";
+const std::string MutableSupport::delmark_column_name = "_INTERNAL_DELMARK";
 
 }
