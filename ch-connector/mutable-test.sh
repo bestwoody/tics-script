c2739ac8d303e5fcfa2c61bd1edca057008b6b90
Liu Cong <liucong@pingcap.com>
2018-03-03 02:48:31 +0800

    Testcase: delete one by one.

--- ch-connector/mutable-test.sh
+++ ch-connector/mutable-test.sh
@@ -2,13 +2,16 @@ function run_file()
 {
 	local dbc="$1"
 	local path="$2"
+	local continue_on_error="$3"
 
 	python mutable-test.py "$dbc" "$path"
 	if [ $? == 0 ]; then
 		echo $path: OK
 	else
 		echo $path: Failed
-		exit 1
+		if [ "$continue_on_error" != "true" ]; then
+			exit 1
+		fi
 	fi
 }
 
@@ -16,6 +19,7 @@ function run_dir()
 {
 	local dbc="$1"
 	local path="$2"
+	local continue_on_error="$3"
 
 	find "$path" -name "*.visual" -depth 1 -type f | sort -V | while read file; do
 		if [ -f "$file" ]; then
@@ -34,7 +38,7 @@ function run_dir()
 
 	find "$path" -name "*.test" -depth 1 -type f | sort -V | while read file; do
 		if [ -f "$file" ]; then
-			run_file "$dbc" "$file"
+			run_file "$dbc" "$file" "$continue_on_error"
 		fi
 	done
 
@@ -44,7 +48,7 @@ function run_dir()
 
 	find "$path" -depth 1 -type d | sort -Vr | while read dir; do
 		if [ -d "$dir" ]; then
-			run_dir "$dbc" "$dir"
+			run_dir "$dbc" "$dir" "$continue_on_error"
 		fi
 	done
 
@@ -57,12 +61,13 @@ function run_path()
 {
 	local dbc="$1"
 	local path="$2"
+	local continue_on_error="$3"
 
 	if [ -f "$path" ]; then
-		run_file "$dbc" "$path"
+		run_file "$dbc" "$path" "$continue_on_error"
 	else
 		if [ -d "$path" ]; then
-			run_dir "$dbc" "$path"
+			run_dir "$dbc" "$path" "$continue_on_error"
 		else
 			echo "error: $path not file nor dir." >&2
 			exit 1
@@ -73,6 +78,7 @@ function run_path()
 target="$1"
 dbc="$2"
 debug="$3"
+continue_on_error="$4"
 
 if [ -z "$target" ]; then
 	target="mutable-test"
@@ -89,4 +95,7 @@ if [ -z "$dbc" ]; then
 	dbc="build/dbms/src/Server/clickhouse client $debug -f PrettyCompactNoEscapes --query"
 fi
 
-run_path "$dbc" "$target"
+if [ -z "$continue_on_error" ]; then
+	continue_on_error="true"
+fi
+run_path "$dbc" "$target" "$continue_on_error"
