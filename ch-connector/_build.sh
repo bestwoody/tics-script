9e18a1b316a8771e10a31ee87344eec3f10d7a2c
Liu Cong <liucong@pingcap.com>
2018-03-28 02:34:25 +0800

    Supress dev warnning when build ch.

--- ch-connector/_build.sh
+++ ch-connector/_build.sh
@@ -13,7 +13,7 @@ make_ch()
 	local old=`pwd`
 	cd "$build_dir"
 
-	cmake "$source_related_dir" -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_C_COMPILER=$CC
+	cmake "$source_related_dir" -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_C_COMPILER=$CC -Wno-dev
 	make -j 4 "$build_target"
 	cd "$old"
 }
