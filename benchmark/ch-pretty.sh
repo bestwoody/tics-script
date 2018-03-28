cd3218583dbe8e2ef22a647d99dd0d801fe61994
Liu Cong <liucong@pingcap.com>
2018-03-28 08:29:34 +0800

    Scripts: auto create current database.

--- benchmark/ch-pretty.sh
+++ benchmark/ch-pretty.sh
@@ -1,3 +1,10 @@
 set -eu
 source _env.sh
+
+"$chbin" client --host="$chserver" --query="create database if not exists $chdb"
+if [ $? != 0 ]; then
+	echo "create database '"$chdb"' failed" >&2
+	exit 1
+fi
+
 "$chbin" client --host "$chserver" -d "$chdb" --query "$@" -f PrettyCompactNoEscapes
