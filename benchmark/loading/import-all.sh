639af7e2df4e354a45fac2168dd6e9a64ffbbd7f
Liu Cong <liucong@pingcap.com>
2018-03-28 08:52:26 +0800

    Script: import-all

---
+++ benchmark/loading/import-all.sh
@@ -0,0 +1,14 @@
+blocks="$1"
+
+set -eu
+
+if [ -z "$blocks" ]; then
+	echo "usage: <bin> block-numbers" >&2
+	exit 1
+fi
+
+source _meta.sh
+
+get_table_names | while read table; do
+	./import.sh "$table" "$blocks"
+done
