09586079d956783b5ac1fff195803f671ce3d21e
Liu Cong <liucong@pingcap.com>
2018-04-09 04:03:22 +0800

    Remove repo definition.

--- ceph-tools/install-el7/install.sh
+++ ceph-tools/install-el7/install.sh
@@ -5,5 +5,3 @@ sudo rpm --import 'https://download.ceph.com/keys/release.asc'
 sudo cp ./ceph.repo /etc/yum.repos.d
 
 sudo yum install ntp ntpdate ntp-doc ceph-deploy -y
-
-ceph-deploy repo --repo-url http://mirrors.ustc.edu.cn/ceph/rpm-luminous/el7/x86_64/ ceph-repo h0
