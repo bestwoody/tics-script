2dca175b1d0261ad6c2609bc1c7416edfb73cb3c
zanmato1984 <zanmato1984@gmail.com>
2018-04-19 19:02:51 +0800

    Pass all tests in maven build.

--- spark-connector/chspark/src/main/scala/org/apache/spark/sql/ch/CHLogicalPlan.scala
+++ spark-connector/chspark/src/main/scala/org/apache/spark/sql/ch/CHLogicalPlan.scala
@@ -15,8 +15,10 @@
 
 package org.apache.spark.sql.ch
 
+
 import org.apache.spark.sql.catalyst.expressions.aggregate.AggregateExpression
 import org.apache.spark.sql.catalyst.expressions.{Expression, NamedExpression, SortOrder}
+import org.apache.spark.sql.catalyst.util
 
 /**
  * IR for a CH query plan, mostly represented in Spark data structures.
@@ -46,25 +48,27 @@ object CHLogicalPlan {
 }
 
 class CHProject(val projectList: Seq[Expression]) {
-  override def toString: String = projectList.mkString("Project [", ", ", "]")
+  override def toString: String = projectList.map(util.toPrettySQL).mkString("Project [", ", ", "]")
 }
 
 class CHFilter(val predicates: Seq[Expression]) {
-  override def toString: String = predicates.mkString("Filter [", ", ", "]")
+  override def toString: String = predicates.map(util.toPrettySQL).mkString("Filter [", ", ", "]")
 }
 
 class CHAggregate(val groupingExpressions: Seq[NamedExpression],
   val aggregateExpressions: Seq[AggregateExpression]) {
   override def toString: String = Seq(
-    if (groupingExpressions.isEmpty) "" else groupingExpressions.mkString("[", ", ", "]"),
-    if (aggregateExpressions.isEmpty) "" else aggregateExpressions.mkString("[", ", ", "]"))
+    if (groupingExpressions.isEmpty) "" else groupingExpressions.map(util.toPrettySQL)
+      .mkString("[", ", ", "]"),
+    if (aggregateExpressions.isEmpty) "" else aggregateExpressions.map(util.toPrettySQL)
+      .mkString("[", ", ", "]"))
     .filter(_.nonEmpty)
     .mkString("Aggregate [", ", ", "]")
 }
 
 class CHTopN(val sortOrders: Seq[SortOrder], val n: Option[Int]) {
   override def toString: String = Seq(
-    if (sortOrders.isEmpty) "" else sortOrders.mkString("[", ", ", "]"),
+    if (sortOrders.isEmpty) "" else sortOrders.map(util.toPrettySQL).mkString("[", ", ", "]"),
     n.getOrElse("").toString)
     .filter(_.nonEmpty)
     .mkString("TopN [", ", ", "]")
