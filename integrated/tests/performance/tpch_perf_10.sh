b5cae961e296fad99a88b030774ccf8c7d62425b
Liu Cong <liucong@pingcap.com>
2019-09-18 00:33:50 +0800

    Fixed tpch perf scale 10 by change ports (#943)

--- integrated/tests/performance/tpch_perf_10.sh
+++ integrated/tests/performance/tpch_perf_10.sh
@@ -4,4 +4,4 @@ source "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/_env.sh"
 source "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/../_base/test_cases.sh"
 auto_error_handle
 
-tpch_perf "${BASH_SOURCE[0]}" 47 10
+tpch_perf "${BASH_SOURCE[0]}" 48 10
