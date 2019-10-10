b52a85b727d1d786cd9626ea5914c46255afda22
JaySon <jayson.hjs@gmail.com>
2019-10-11 01:47:23 +0800

    ti.sh:  CH-client run queries from file (#988)
    * Run tpch query with ch client
    * add ch-queries according to mysql-queries
    * fix the date of spark-q1

--- integrated/ops/ti.sh.cmds/remote/ch.sh
+++ integrated/ops/ti.sh.cmds/remote/ch.sh
@@ -15,7 +15,7 @@ function cmd_ti_ch()
 	fi
 
 	if [ -z "${1+x}" ]; then
-		echo '[cmd ch] <cmd> query [database] [print-format] [ch-args]' >&2
+		echo '[cmd ch] <cmd> query-str-or-file-path [database] [print-format] [ch-args]' >&2
 		return
 	fi
 
@@ -37,12 +37,16 @@ function cmd_ti_ch()
 	fi
 
 	shift 3
-
+	if [ -f "${query}" ]; then
+		local query_str="`cat ${query}`"
+	else
+		local query_str="${query}"
+	fi
 	local port=`get_value "${dir}/proc.info" 'tcp_port'`
 	if [ -z "${1+x}" ]; then
-		"${dir}/tiflash" client --host="${host}" --port="${port}" -d "${db}" -f "${format}" --query="${query}"
+		"${dir}/tiflash" client --host="${host}" --port="${port}" -d "${db}" -f "${format}" --query="${query_str}"
 	else
-		"${dir}/tiflash" client --host="${host}" --port="${port}" -d "${db}" -f "${format}" --query="${query}" "${@}"
+		"${dir}/tiflash" client --host="${host}" --port="${port}" -d "${db}" -f "${format}" --query="${query_str}" "${@}"
 	fi
 }
 
