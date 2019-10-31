b2737a043b2ff394dd1303a077f88bf5eb3f3bd9
Liu Cong <liucong@pingcap.com>
2019-11-01 05:14:24 +0800

    Added ops cmd: learner, can add hint to query and run on tiflash (#1059)

---
+++ integrated/ops/ti.sh.cmds/learner.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+function cmd_ti_mysql()
+{
+	local index="${1}"
+	local mod_name="${2}"
+	local dir="${3}"
+	local conf_rel_path="${4}"
+	local host="${5}"
+
+	if [ "${mod_name}" != 'tidb' ]; then
+		return
+	fi
+
+	if [ -z "${6+x}" ]; then
+		echo '[cmd mysql] usage: <cmd> query_str_or_file_path [database]' >&2
+		return
+	fi
+
+	local query="${6}"
+	if [ -z "${query}" ]; then
+		return
+	fi
+
+	if [ -z "${7+x}" ]; then
+		local db='mysql'
+	else
+		local db="${7}"
+	fi
+
+	local port=`get_value "${dir}/proc.info" 'tidb_port'`
+	if [ -z "${port}" ]; then
+		echo '[cmd mysql] get port failed' >&2
+		return 1
+	fi
+
+	if [ -f "${query}" ]; then
+		local query=`cat "${query}" | tr -s "\n" " "`
+	fi
+
+	local query=`echo "${query}" | tr 'A-Z' 'a-z'`
+	local query=`replace_substr "${query}" 'select' 'select /*+ read_from_storage(tiflash[lineitem]) */'`
+
+	mysql -h "${host}" -P "${port}" -u root --database="${db}" --comments -e "${query}"
+}
+
+cmd_ti_mysql "${@}"
