b5d7fe7123960bb30e5803b40337f04796007732
Liu Cong <liucong@pingcap.com>
2019-11-04 02:27:37 +0800

    Fixed ops cmd: ci/release (#1078)

--- integrated/ops/ti.sh.cmds/global/ci/release.sh
+++ integrated/ops/ti.sh.cmds/global/ci/release.sh
@@ -12,9 +12,9 @@ function cmd_ci_release()
 	rm -f "${file}"
 	"${ti}" new "${file}" 'delta=-6' "dir=${dir}"
 
-	"${ti}" repeat 10 'up:tpch/load 0.1 all:tpch/ch:tpch/tikv'
+	"${ti}" "${file}" repeat 10 'up:tpch/load 0.1 all:tpch/ch:tpch/tikv'
 	# TODO: remote 'sleep' after FLASH-635 is addressed
-	"${ti}" repeat 10 'kill/storage:up:sleep 300:tpch/ch:tpch/tikv'
+	"${ti}" "${file}" repeat 10 'kill/storage:up:sleep 300:tpch/ch:tpch/tikv'
 
 	"${ti}" "${file}" must burn doit
 	rm -f "${file}"
