6f9e46e073aa5e2d8beead0d80063fb02a84e73b
lidezhu <47731263+lidezhu@users.noreply.github.com>
2019-11-04 15:43:54 +0800

    Revert "Revert "Fix learner label in CHSpark (#991)" (#994)" (#1080)
    This reverts commit 3b9f5dc1862bc4d642dc8516a890cb66df5594b1.

--- computing/chspark/src/main/java/com/pingcap/theflash/SparkCHClientSelect.java
+++ computing/chspark/src/main/java/com/pingcap/theflash/SparkCHClientSelect.java
@@ -265,7 +265,10 @@ public class SparkCHClientSelect implements Closeable, Iterator<CHColumnBatch> {
 
         long storeId =
             CHUtil.getRegionLearnerPeerByLabel(
-                    region, tiSession.getRegionManager(), "zone", "engine")
+                    region,
+                    tiSession.getRegionManager(),
+                    CHUtil.LearnerLabelKey(),
+                    CHUtil.LearnerLabelValue())
                 .get()
                 .getStoreId();
         tiSession.getRegionManager().onRequestFail(region.getId(), storeId);
@@ -291,7 +294,10 @@ public class SparkCHClientSelect implements Closeable, Iterator<CHColumnBatch> {
                   .getRegionManager()
                   .getStoreById(
                       CHUtil.getRegionLearnerPeerByLabel(
-                              region, tiSession.getRegionManager(), "zone", "engine")
+                              region,
+                              tiSession.getRegionManager(),
+                              CHUtil.LearnerLabelKey(),
+                              CHUtil.LearnerLabelValue())
                           .get()
                           .getStoreId())
                   .getAddress()
