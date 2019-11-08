69670042cf19073e80ee94665b2ecbad1705e8bf
Liu Cong <liucong@pingcap.com>
2019-11-08 17:23:56 +0800

    Added ops func: trim_host (#1116)

--- integrated/_base/pipe.sh
+++ integrated/_base/pipe.sh
@@ -5,3 +5,10 @@ function uncolor()
 	sed 's/\x1B\[[0-9;]\+[A-Za-z]//g'
 }
 export -f uncolor
+
+function trim_host()
+{
+	local here="`cd $(dirname ${BASH_SOURCE[0]}) && pwd`"
+	python "${here}/trim_host.py"
+}
+export -f trim_host
