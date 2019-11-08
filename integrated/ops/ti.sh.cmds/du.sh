7139659a8e9a12f9e38dfb7033eb36e0ba6bf918
Liu Cong <liucong@pingcap.com>
2019-11-08 18:08:31 +0800

    Improved ops cmd: du, show total size and data size both (#1118)

--- integrated/ops/ti.sh.cmds/du.sh
+++ integrated/ops/ti.sh.cmds/du.sh
@@ -7,12 +7,26 @@ function cmd_ti_du()
 	local dir="${3}"
 	local conf_rel_path="${4}"
 
-	if [ -d "${dir}" ]; then
-		local res=`du -sh "${dir}" | awk '{print $1}'`
-	else
-		local res="MISSED"
+	if [ "${mod_name}" == 'pd' ] || [ "${mod_name}" == 'tikv' ] || [ "${mod_name}" == 'rngine' ]; then
+		local data_dir="${dir}/data"
+	elif [ "${mod_name}" == 'tiflash' ]; then
+		local data_dir="${dir}/db"
 	fi
-	echo "${mod_name} #${index} (${dir}) ${res}"
+
+	if [ ! -z "${data_dir+x}" ] && [ -d "${data_dir}" ]; then
+		local data=`du -sh "${data_dir}" | awk '{print $1}'`
+		local data="${data}/"
+	else
+		local data=''
+	fi
+
+	if [ ! -d "${dir}" ]; then
+		local total='MISSED'
+	else
+		local total=`du -sh "${dir}" | awk '{print $1}'`
+	fi
+
+	echo "${mod_name} #${index} (${dir}) ${data}${total}"
 }
 
 cmd_ti_du "${@}"
