8a923093ebba63d629a8c91ec12ab5ddee27cff4
zyguan <zhongyangguan@gmail.com>
2019-11-12 12:03:38 +0800

    Improved ci: run tests in parallel (#1120)

--- .ci/integration_test.groovy
+++ .ci/integration_test.groovy
@@ -1,88 +1,124 @@
-def label = "test-tiflash"
-
-
 catchError {
 
     def util = load('util.groovy')
 
-    podTemplate(name: label, label: label, instanceCap: 3, containers: [
-        containerTemplate(name: 'dockerd', image: 'docker:18.09.6-dind', privileged: true,
-                resourceRequestCpu: '2000m', resourceRequestMemory: '8Gi'),
-        containerTemplate(name: 'docker', image: 'hub.pingcap.net/tiflash/docker:build-essential-java',
-                envVars: [ envVar(key: 'DOCKER_HOST', value: 'tcp://localhost:2375')],
-                alwaysPullImage: true, ttyEnabled: true, command: 'cat'),
-        containerTemplate(name: 'docker-ops-ci', image: 'hub.pingcap.net/tiflash/ops-ci:v10',
-                envVars: [ envVar(key: 'DOCKER_HOST', value: 'tcp://localhost:2375') ],
-                alwaysPullImage: true, ttyEnabled: true, command: 'cat'),
-    ]) {
-        node(label) {
-
-            def ticsTag = ({
-                def m = params.ghprbCommentBody =~ /tics\s*=\s*([^\s\\]+)(\s|\\|$)/
-                if (m) {
-                    return "${m.group(1)}"
-                }
-                return params.ticsTag ?: params.ghprbTargetBranch ?: 'raft'
-            }).call()
-
-            echo "ticsTag=${params.ticsTag} ghprbTargetBranch=${params.ghprbTargetBranch} ghprbCommentBody=${params.ghprbCommentBody}"
-
-            stage("Checkout") {
-                container("docker") {
-                    sh "chown -R 1000:1000 ./"
-                }
-                dir("tiflash") {
-                    util.checkoutTiFlash("${params.ghprbActualCommit}", "${params.ghprbPullId}")
-                }
-                dir("tispark") {
-                    git url: "https://github.com/pingcap/tispark.git", branch: "tiflash-ci-test"
-                    container("docker") {
-                        sh """
-                        archive_url=${FILE_SERVER_URL}/download/builds/pingcap/tiflash/cache/tiflash-m2-cache_latest.tar.gz
-                        if [ ! -d /root/.m2 ]; then curl -sL \$archive_url | tar -zx -C /root || true; fi
-                        """
-                        sh "mvn install -Dmaven.test.skip=true"
-                    }
-                }
-            }
-            dir("tiflash/integrated") {
-                stage("OPS TI Test") {
-                    container("docker-ops-ci") {
-                        if (fileExists("tests/ci/jenkins.sh")) {
-                            try {
-                                sh "tests/ci/jenkins.sh"
-                            } catch (err) {
-                                throw err
-                            }
-                        }
-                    }
-                }
-            }
-            dir("tiflash/tests/maven") {
-                stage("Test") {
-                    container("docker") {
-
-                        def firstTrial = true
-                        retry(20) {
-                            if (firstTrial) {
-                                firstTrial = false
-                            } else {
-                                sleep time: 5, unit: "SECONDS"
-                            }
-                            sh "docker pull hub.pingcap.net/tiflash/tics:$ticsTag"
-                        }
-
-                        try {
-                            sh "TAG=$ticsTag sh -xe run.sh"
-                        } catch(e) {
-                            sh "for f in \$(find log -name '*.log'); do echo \"LOG: \$f\"; tail -500 \$f; done"
-                            throw e
-                        }
-                    }
-                }
-            }
+    def ticsTag = ({
+        def m = params.ghprbCommentBody =~ /tics\s*=\s*([^\s\\]+)(\s|\\|$)/
+        if (m) {
+            return "${m.group(1)}"
         }
-    }
+        return params.ticsTag ?: params.ghprbTargetBranch ?: 'raft'
+    }).call()
+
+    echo "ticsTag=${params.ticsTag} ghprbTargetBranch=${params.ghprbTargetBranch} ghprbCommentBody=${params.ghprbCommentBody}"
+
+    def tasks = {}
+
+    parallel(
+        "OPS TI Test": {
+            def label = "test-tiflash-ops-ti"
+
+            podTemplate(name: label, label: label, instanceCap: 3, containers: [
+                containerTemplate(name: 'dockerd', image: 'docker:18.09.6-dind', privileged: true,
+                        resourceRequestCpu: '2000m', resourceRequestMemory: '8Gi'),
+                containerTemplate(name: 'docker', image: 'hub.pingcap.net/tiflash/docker:build-essential-java',
+                        envVars: [ envVar(key: 'DOCKER_HOST', value: 'tcp://localhost:2375')],
+                        alwaysPullImage: true, ttyEnabled: true, command: 'cat'),
+                containerTemplate(name: 'docker-ops-ci', image: 'hub.pingcap.net/tiflash/ops-ci:v10',
+                        envVars: [ envVar(key: 'DOCKER_HOST', value: 'tcp://localhost:2375') ],
+                        alwaysPullImage: true, ttyEnabled: true, command: 'cat'),
+            ]) {
+                node(label) {
+                    stage("Checkout") {
+                        container("docker") {
+                            sh "chown -R 1000:1000 ./"
+                        }
+                        dir("tiflash") {
+                            util.checkoutTiFlash("${params.ghprbActualCommit}", "${params.ghprbPullId}")
+                        }
+                        dir("tispark") {
+                            git url: "https://github.com/pingcap/tispark.git", branch: "tiflash-ci-test"
+                            container("docker") {
+                                sh """
+                                archive_url=${FILE_SERVER_URL}/download/builds/pingcap/tiflash/cache/tiflash-m2-cache_latest.tar.gz
+                                if [ ! -d /root/.m2 ]; then curl -sL \$archive_url | tar -zx -C /root || true; fi
+                                """
+                                sh "mvn install -Dmaven.test.skip=true"
+                            }
+                        }
+                    }
+                    dir("tiflash/integrated") {
+                        stage("OPS TI Test") {
+                            container("docker-ops-ci") {
+                                if (fileExists("tests/ci/jenkins.sh")) {
+                                    try {
+                                        sh "tests/ci/jenkins.sh"
+                                    } catch (err) {
+                                        throw err
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        "Integration Test": {
+            def label = "test-tiflash-integration"
+
+            podTemplate(name: label, label: label, instanceCap: 3, containers: [
+                containerTemplate(name: 'dockerd', image: 'docker:18.09.6-dind', privileged: true,
+                        resourceRequestCpu: '2000m', resourceRequestMemory: '8Gi'),
+                containerTemplate(name: 'docker', image: 'hub.pingcap.net/tiflash/docker:build-essential-java',
+                        envVars: [ envVar(key: 'DOCKER_HOST', value: 'tcp://localhost:2375')],
+                        alwaysPullImage: true, ttyEnabled: true, command: 'cat'),
+            ]) {
+                node(label) {
+                    stage("Checkout") {
+                        container("docker") {
+                            sh "chown -R 1000:1000 ./"
+                        }
+                        dir("tiflash") {
+                            util.checkoutTiFlash("${params.ghprbActualCommit}", "${params.ghprbPullId}")
+                        }
+                        dir("tispark") {
+                            git url: "https://github.com/pingcap/tispark.git", branch: "tiflash-ci-test"
+                            container("docker") {
+                                sh """
+                                archive_url=${FILE_SERVER_URL}/download/builds/pingcap/tiflash/cache/tiflash-m2-cache_latest.tar.gz
+                                if [ ! -d /root/.m2 ]; then curl -sL \$archive_url | tar -zx -C /root || true; fi
+                                """
+                                sh "mvn install -Dmaven.test.skip=true"
+                            }
+                        }
+                    }
+                    dir("tiflash/tests/maven") {
+                        stage("Test") {
+                            container("docker") {
+
+                                def firstTrial = true
+                                retry(20) {
+                                    if (firstTrial) {
+                                        firstTrial = false
+                                    } else {
+                                        sleep time: 5, unit: "SECONDS"
+                                    }
+                                    sh "docker pull hub.pingcap.net/tiflash/tics:$ticsTag"
+                                }
+
+                                try {
+                                    sh "TAG=$ticsTag sh -xe run.sh"
+                                } catch(e) {
+                                    sh "for f in \$(find log -name '*.log'); do echo \"LOG: \$f\"; tail -500 \$f; done"
+                                    throw e
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+    )
 
 }
 
