24531876941eff153d61c8682e7df9ed8c35e8da
lidezhu <47731263+lidezhu@users.noreply.github.com>
2020-01-19 19:28:30 +0800

    Add beeline/raw command and refactor beeline command (#1239)
    * Add beeline/raw command and refactor beeline command

--- integrated/ops/ti.sh.cmds/beeline.sh
+++ integrated/ops/ti.sh.cmds/beeline.sh
@@ -14,25 +14,52 @@ function cmd_ti_beeline()
 		return
 	fi
 
-	if [ -z "${2+x}" ]; then
-		echo '[cmd beeline] <cmd> database beeline-args' >&2
-		return 1
-	fi
-	local db="${1}"
-	if [ "${db}" == "-e" ]; then
-		echo '[cmd beeline] <cmd> database beeline-args' >&2
+	if [ -z "${1+x}" ]; then
+		echo '[cmd beeline] usage: <cmd> query_str_or_file_path [database] [show_elapsed=true]' >&2
 		return 1
 	fi
 
-	shift 1
+	local query="${1}"
+	if [ -z "${query}" ]; then
+		return 1
+	fi
+
+	if [ -z "${2+x}" ]; then
+		local db='test'
+	else
+		local db="${2}"
+	fi
+
+	if [ -z "${3+x}" ]; then
+		local show_elapsed='true'
+	else
+		local show_elapsed="${3}"
+	fi
+	if [ "${show_elapsed}" != 'false' ] && [ "${show_elapsed}" != 'true' ]; then
+		echo "[cmd mysql] show_elapsed should be 'true|false', got '${show_elapsed}'" >&2
+		return 1
+	fi
 
 	local port=`get_value "${dir}/proc.info" 'thriftserver_port'`
+	if [ -z "${port}" ]; then
+		echo '[cmd mysql] get port failed' >&2
+		return 1
+	fi
 
-	prepare_spark_env
-	local start_time=`timer_start`
-	${dir}/spark/bin/beeline --verbose=false --silent=true -u "jdbc:hive2://${host}:${port}/${db}" "${@}"
-	local elapsed=`timer_end "${start_time}"`
-	echo "elapsed: ${elapsed}"
+	if [ "${show_elapsed}" == 'true' ]; then
+		local start_time=`timer_start`
+	fi
+
+	if [ -f "${query}" ]; then
+		${dir}/spark/bin/beeline --verbose=false --silent=true -u "jdbc:hive2://${host}:${port}/${db}" -f "${query}"
+	else
+		${dir}/spark/bin/beeline --verbose=false --silent=true -u "jdbc:hive2://${host}:${port}/${db}" -e "${query}"
+	fi
+
+	if [ "${show_elapsed}" == 'true' ]; then
+		local elapsed=`timer_end "${start_time}"`
+		echo "elapsed: ${elapsed}"
+	fi
 }
 
 set -euo pipefail
