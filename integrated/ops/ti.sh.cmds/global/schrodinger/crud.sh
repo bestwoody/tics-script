a570f10ee526b4441b7866ee70398698cf07c134
lidezhu <47731263+lidezhu@users.noreply.github.com>
2020-02-06 12:35:15 +0800

    Add region scheduler config in schrodinger test (#1246)
    * Support region scheduler option in schrodinger/bank

--- integrated/ops/ti.sh.cmds/global/schrodinger/crud.sh
+++ integrated/ops/ti.sh.cmds/global/schrodinger/crud.sh
@@ -3,10 +3,38 @@
 function cmd_ti_global_schrodinger_crud()
 {
 	if [ -z "${1+x}" ] || [ -z "${1}" ]; then
+		local enable_region_merge='false'
+	else
+		local enable_region_merge="${1}"
+		if [ "${enable_region_merge}" != 'false' ] && [ "${enable_region_merge}" != 'true' ]; then
+			echo "[cmd schrodinger/bank] value of enable_region_merge should be true or false" >&2
+			return 1
+		fi
+	fi
+	if [ -z "${2+x}" ] || [ -z "${2}" ]; then
+		local enable_shuffle_region='false'
+	else
+		local enable_shuffle_region="${2}"
+		if [ "${enable_shuffle_region}" != 'false' ] && [ "${enable_shuffle_region}" != 'true' ]; then
+			echo "[cmd schrodinger/bank] value of enable_shuffle_region should be true or false" >&2
+			return 1
+		fi
+	fi
+	if [ -z "${3+x}" ] || [ -z "${3}" ]; then
+		local enable_shuffle_leader='false'
+	else
+		local enable_shuffle_leader="${3}"
+		if [ "${enable_shuffle_leader}" != 'false' ] && [ "${enable_shuffle_leader}" != 'true' ]; then
+			echo "[cmd schrodinger/bank] value of enable_shuffle_leader should be true or false" >&2
+			return 1
+		fi
+	fi
+	if [ -z "${4+x}" ] || [ -z "${4}" ]; then
 		local dir='/tmp/ti/schrodinger/crud'
 	else
-		local dir="${1}"
+		local dir="${4}"
 	fi
+	
 	local ti="${integrated}/ops/ti.sh"
 
 	mkdir -p "${dir}"
@@ -17,6 +45,21 @@ function cmd_ti_global_schrodinger_crud()
 
 	"${ti}" "${file}" must burn
 	"${ti}" "${file}" run
+
+	if [ "${enable_region_merge}" == 'true' ]; then
+		"${ti}" "${file}" 'pd/ctl_raw' 'scheduler' 'add' 'random-merge-scheduler' 1>/dev/null 2>&1
+		"${ti}" "${file}" 'pd/ctl_raw' 'config' 'set' 'merge-schedule-limit' 8 1>/dev/null 2>&1
+	fi
+	if [ "${enable_shuffle_region}" == 'true' ]; then
+		"${ti}" "${file}" 'pd/ctl_raw' 'scheduler' 'add' 'shuffle-region-scheduler' 1>/dev/null 2>&1
+	fi
+	if [ "${enable_shuffle_leader}" == 'true' ]; then
+		"${ti}" "${file}" 'pd/ctl_raw' 'scheduler' 'add' 'shuffle-leader-scheduler' 1>/dev/null 2>&1
+	fi
+	echo "Enable region merge: ${enable_region_merge}"
+	echo "Enable shuffle region: ${enable_shuffle_region}"
+	echo "Enable shuffle leader: ${enable_shuffle_leader}"
+
 	"${ti}" "${file}" schrodinger/crud
 
 	print_hhr
@@ -24,5 +67,8 @@ function cmd_ti_global_schrodinger_crud()
 }
 
 source "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/_env.sh"
-auto_error_handle
-cmd_ti_global_schrodinger_crud
+error_handle="$-"
+set +eu
+cmd_args=("${@}")
+restore_error_handle_flags "${error_handle}"
+cmd_ti_global_schrodinger_crud "${cmd_args[@]}"
