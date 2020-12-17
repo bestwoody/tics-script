c420d6b35fef7c5eac3518a7b763dc94df795678
JaySon <tshent@qq.com>
2020-12-17 02:20:36 -0600

    Use latest binary for running integration tests (#1619)
    Signed-off-by: JaySon-Huang <tshent@qq.com>

--- integrated/tests/ci/jenkins.sh
+++ integrated/tests/ci/jenkins.sh
@@ -5,4 +5,23 @@ auto_error_handle
 
 cd "${integrated}"
 
+## Download the latest binaries
+echo "Downloading binaries ... "
+BIN_PATHS_CFG_FILE="${integrated}/conf/bin.paths"
+BIN_PATHS_CFG_FILE_SAVED="${BIN_PATHS_CFG_FILE}.saved"
+
+if [ -f "${BIN_PATHS_CFG_FILE}" ]; then
+    mv "${BIN_PATHS_CFG_FILE}" "${BIN_PATHS_CFG_FILE_SAVED}"
+fi
+cp "${integrated}/tests/ci/conf/bin.paths" "${BIN_PATHS_CFG_FILE}"
+
+ops/ti.sh download "${integrated}/tests/ci/conf/download.ti" "${integrated}/binary"
+ops/ti.sh "${integrated}/tests/ci/conf/download.ti" burn : up : ver : burn
+
+echo "Running tests ... "
 ops/ti.sh ci/jenkins
+
+# Restore config file
+if [ -f "${BIN_PATHS_CFG_FILE_SAVED}" ]; then
+    mv "${BIN_PATHS_CFG_FILE_SAVED}" "${BIN_PATHS_CFG_FILE}"
+fi
