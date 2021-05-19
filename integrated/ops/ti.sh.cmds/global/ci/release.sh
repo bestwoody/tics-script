f842196f2788199aa1b9356344caadee8e3cda51
Zhuhe Fang <fzhedu@gmail.com>
2021-05-19 13:40:09 +0800

    disable run-generate-tests for randgen-mpp (#1716)

--- integrated/ops/ti.sh.cmds/global/ci/release.sh
+++ integrated/ops/ti.sh.cmds/global/ci/release.sh
@@ -13,11 +13,11 @@ function cmd_ti_global_ci_release()
 	"${ti}" "${file}" burn : up
 	"${ti}" "${file}" ci/release
 	## randgen-mpp only runs daily due to heavy workloads
-	"${ti}" "${file}" ci/fullstack randgen-mpp
+	"${ti}" "${file}" ci/fullstack randgen-mpp false
 	## change parameters to run the test again
 	## force shuffle hash join rather than broadcast join
 	"${ti}" "${file}" mysql "set @@global.tidb_broadcast_join_threshold_size=0; set @@global.tidb_broadcast_join_threshold_count=0;" test false
-	"${ti}" "${file}" ci/fullstack randgen-mpp
+	"${ti}" "${file}" ci/fullstack randgen-mpp false
 	"${ti}" "${file}" must burn
 
 	rm -f "${file}"
